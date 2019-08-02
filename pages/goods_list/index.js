f0a5da193b24e8d4492a0e9fa590ac20acb17171
gillill <466660055@qq.com>
2019-08-02 16:25:59 +0800

    商品列表引用组件并数据动态渲染

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,38 @@
-// pages/goods_list/index.js
+import { request } from "../../request/index.js";
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tabs: [
+      { id: 0, title: '综合', isActive: true },
+      { id: 1, title: '销量', isActive: false },
+      { id: 2, title: '价格', isActive: false },
+    ],
+    goodsList: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  QueryParams: {
+    query: '',
+    cid: '',
+    pagenum: 1,
+    pagesize: 10
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onLoad(options) {
+    // console.log(options)
+    this.QueryParams.cid = options.cid
+    this.getGoodsList()
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  getGoodsList() {
+    request({ url: '/goods/search', data: this.QueryParams })
+    .then(res=>{
+      console.log(res)
+      this.setData({
+        goodsList: res.goods
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleTitleChange(e) {
+    const { index } = e.detail
+    let { tabs } = this.data
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false)
+    this.setData({ tabs })
   }
 })
