50b29364941761ad96efe23b2df28a508703d8e1
gillill <466660055@qq.com>
2019-08-02 15:32:31 +0800

    实现分类数据缓存效果

--- pages/category/index.js
+++ pages/category/index.js
@@ -3,27 +3,57 @@ Page({
   data: {
     leftMenuList: [],
     rightGoodsList: [],
-    currentIndex: 0
+    currentIndex: 0,
+    scrollTop: 0
   },
+  Cates: [],
   onLoad() {
-    this.getCategoryList();
+    let cates = wx.setStorageSync('cates');
+    if (!cates) {
+      this.getCategoryList();
+    } else {
+      if (Date.now() - cates.time > 1000 * 20) {
+        this.getCategoryList();
+      } else {
+        this.Cates = cates.data
+        let leftMenuList = this.Cates.map((v, i) => ({
+          cat_name: v.cat_name, cat_id: v.cat_id
+        }))
+        let rightGoodsList = this.Cates[0].children
+        this.setData({
+          leftMenuList,
+          rightGoodsList
+        })
+      }
+    }
   },
   getCategoryList() {
     request({
       url: '/categories'
     })
       .then(result => {
-        let leftMenuList = result.map((v, i) => ({
+        this.Cates = result
+        wx.setStorageSync('cates', { time: Date.now(), data: this.Cates })
+        let leftMenuList = this.Cates.map((v, i) => ({
           cat_name: v.cat_name, cat_id: v.cat_id
         }))
-        let rightGoodsList = result[0].children
+        let rightGoodsList = this.Cates[0].children
         this.setData({
           leftMenuList,
           rightGoodsList
-          
+
         })
         // console.log(leftMenuList)
         // console.log(rightGoodsList)
       })
+  },
+  handleMenuChange(e) {
+    const { index } = e.currentIndex.dataset
+    let rightGoodsList = this.Cates[index].children
+    this.setData({
+      currentIndex: index,
+      rightGoodsList,
+      scrollTop
+    })
   }
 })
