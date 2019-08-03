e3cc3bc1a7526826649380f367210a1dd254a133
gillill <466660055@qq.com>
2019-08-03 22:30:51 +0800

    购物车收货地址+静态页面

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,6 +1,9 @@
 import regeneratorRuntime from '../../lib/runtime/runtime';
 import { getSetting, openSetting, chooseAddress } from "../../utils/asyncWx";
 Page({
+  data: {
+    address: {}
+  },
   async handleChooseAddress() {
     // 1 获取授权信息
     const res1 = await getSetting();
@@ -14,8 +17,14 @@ Page({
       // 2.2 诱导用户 打开授权页面
       await openSetting();
       // 2.3 获取收货地址
-      const res2 = await chooseAddress();
-      console.log(res2);
+
     }
+    const address = await chooseAddress()
+    address.all = address.provinceName + address.cityName + address.countyName + address.detailInfo
+    wx.setStorageSync('address', address)
+  },
+  onShow() {
+    const address = wx.getStorageSync('address') || {}
+    this.setData({ address })
   }
 })
